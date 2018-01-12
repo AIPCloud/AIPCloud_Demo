c17947ab26380bbb23bbf8bfadb880bfb295aad8
Taqiyeddine Sakmeche <alaxa27@gmail.com>
2018-01-12 01:27:19 +0100

    launch server script update.

--- launch_server.sh
+++ launch_server.sh
@@ -2,8 +2,8 @@
 
 source ./.env/bin/activate
 
-python3 sentence_intent/server.py
-python3 sentence_sentiment/server.py
-python3 speaker_change_detection/server.py
-python3 speaker_emotion/server.py
-python3 speech_to_text/server.py
+python3 sentence_intent/server.py&
+python3 sentence_sentiment/server.py&
+python3 speaker_change_detection/server.py&
+python3 speaker_emotion/server.py&
+python3 speech_to_text/server.py&
